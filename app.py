47fe8ded158bf2bebdc36bdce96e62b693607b34
unknown <Richard Liu@R-TRINITYFORCE.MIT.EDU>
2018-09-15 19:12:20 -0400

    make it better

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, request, jsonify
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET'])
+def home():
+    return "Hello World!"
+
+@app.route('/api/places', methods=['GET'])
+def api_id():
+    # Check if place data was provided as part of the URL.
+    # If place data is provided, assign it to a variable.
+    # If no place data is provided, display an error in the browser.
+    if 'from_place' in request.args:
+        from_place = request.args['from_place']
+    else:
+        return "Error: No [from place] field provided. Please specify a [from place]."
+    
+    if 'to_place' in request.args:
+        to_place = request.args['to_place']
+    else:
+        return "Error: No [to place] field provided. Please specify a [to place]."
+
+    # Create an empty list for our results
+    results = []
+
+    # Loop through the data and match results that fit the requested ID.
+    # IDs are unique, but other fields might return many results
+    # for book in books:
+    #     if book['id'] == id:
+    #         results.append(book)
+
+    # Use the jsonify function from Flask to convert our list of
+    # Python dictionaries to the JSON format.
+    # return jsonify(results)
+
+    price_data = [[datetime(2018, 9, 15, 17, 14, 41, 814154), datetime(2018, 9, 16, 17, 14, 41, 814154), datetime(2018, 9, 17, 17, 14, 41, 814154), datetime(2018, 9, 18, 17, 14, 41, 814154), datetime(2018, 9, 19, 17, 14, 41, 814154), datetime(2018, 9, 20, 17, 14, 41, 814154), datetime(2018, 9, 21, 17, 14, 41, 814154), datetime(2018, 9, 22, 17, 14, 41, 814154)], [165.2, 165.2, 165.2, 81.2, 81.2, 81.2, 81.2, 65.2]]
+    
+    return jsonify(price_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
