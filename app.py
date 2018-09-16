e4788214f51631593cf5f4379d48919859afef96
unknown <Richard Liu@R-TRINITYFORCE.MIT.EDU>
2018-09-15 20:06:45 -0400

    make it better

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
+from time import sleep
 from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route("/", methods=['GET'])
 def home():
-    return "Hello World!"
+    return render_template("home.html")
 
 @app.route('/api/places', methods=['GET'])
 def api_id():
@@ -14,13 +15,19 @@ def api_id():
     # If no place data is provided, display an error in the browser.
     if 'from_place' in request.args:
         from_place = request.args['from_place']
+        if from_place == '':
+            return "Error: No [from place] field provided. Please specify a [from place]."
     else:
         return "Error: No [from place] field provided. Please specify a [from place]."
     
     if 'to_place' in request.args:
         to_place = request.args['to_place']
+        if to_place == '':
+            return "Error: No [to place] field provided. Please specify a [to place]."
     else:
         return "Error: No [to place] field provided. Please specify a [to place]."
+
+    # return "{} {}".format(from_place, to_place)
 
     # Create an empty list for our results
     results = []
@@ -37,6 +44,8 @@ def api_id():
 
     price_data = [[datetime(2018, 9, 15, 17, 14, 41, 814154), datetime(2018, 9, 16, 17, 14, 41, 814154), datetime(2018, 9, 17, 17, 14, 41, 814154), datetime(2018, 9, 18, 17, 14, 41, 814154), datetime(2018, 9, 19, 17, 14, 41, 814154), datetime(2018, 9, 20, 17, 14, 41, 814154), datetime(2018, 9, 21, 17, 14, 41, 814154), datetime(2018, 9, 22, 17, 14, 41, 814154)], [165.2, 165.2, 165.2, 81.2, 81.2, 81.2, 81.2, 65.2]]
     
+    sleep(1)
+
     return jsonify(price_data)
 
 if __name__ == '__main__':
--- static/js/home.js
+++ static/js/home.js
@@ -1,293 +1,72 @@
-var updateGraph = function() {
-    var xmlHttp = new XMLHttpRequest(); // object to make GET request
-    xmlHttp.onreadystatechange = function() { // function that is called once we get the response
-      var price_data = JSON.parse(xmlHttp.responseText);
-    //   var price_data = xmlHttp.responseText.slice(1, -1);
+var submitPlaces = function(from_place, to_place) {
+  var xmlHttp = new XMLHttpRequest(); // object to make GET request
+  xmlHttp.onreadystatechange = function() { // function that is called once we get the response
+    responseText = xmlHttp.responseText;
+    if(responseText.substring(0,5) == "Error") {
+      console.log(responseText);
+    }
+    else {
+      var price_data = JSON.parse(this.responseText);
       console.log(price_data)
-      console.log(price_data[0])
-      console.log(price_data[1])
-    //   document.getElementById('fun-fact').innerHTML = response;
-      var data = {
-        labels: price_data[0],
-        datasets: [{
-            label: "Dataset #1",
-            backgroundColor: "rgba(255,99,132,0.2)",
-            borderColor: "rgba(255,99,132,1)",
-            borderWidth: 2,
-            hoverBackgroundColor: "rgba(255,99,132,0.4)",
-            hoverBorderColor: "rgba(255,99,132,1)",
-            data: price_data[1],
-        }]
-        };
+      updateGraph(price_data)
+    }
+  };
+  var requestUrl = `api/places?from_place=${from_place}&to_place=${to_place}`;
+  xmlHttp.open('GET', requestUrl, true);
+  xmlHttp.send(null);
+  if(from_place.length>0 && to_place.length>0) {
+    updateGraph([[0,0,0],[0,0,0]]);
+  }
+}
 
-        var options = {
-        maintainAspectRatio: false,
-        scales: {
-            yAxes: [{
-            stacked: true,
-            gridLines: {
-                display: true,
-                color: "rgba(255,99,132,0.2)"
-            }
-            }],
-            xAxes: [{
-            gridLines: {
-                display: false
-            }
-            }]
+var updateGraph = function(price_data) {
+  var data = {
+    labels: price_data[0],
+    datasets: [{
+        label: "Dataset #1",
+        backgroundColor: "rgba(255,99,132,0.2)",
+        borderColor: "rgba(255,99,132,1)",
+        borderWidth: 2,
+        hoverBackgroundColor: "rgba(255,99,132,0.4)",
+        hoverBorderColor: "rgba(255,99,132,1)",
+        data: price_data[1],
+    }]
+  };
+
+  var options = {
+    maintainAspectRatio: false,
+    scales: {
+      yAxes: [{
+        stacked: true,
+        gridLines: {
+          display: true,
+          color: "rgba(255,99,132,0.2)"
+        } 
+      }],
+      xAxes: [{
+        gridLines: {
+          display: false
         }
-        };
+      }]
+    }
+  };
 
-        Chart.Line('chart', {
-        options: options,
-        data: data
-        });
-    };
-    xmlHttp.open('GET', 'api/places', true);
-    xmlHttp.send(null);
-  }
-  
-  document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('graph').addEventListener('click', function() {
-        updateGraph();
-    });
+  Chart.Line('chart', {
+    options: options,
+    data: data
   });
-
-  var updateGraph = function() {
-    var xmlHttp = new XMLHttpRequest(); // object to make GET request
-    xmlHttp.onreadystatechange = function() { // function that is called once we get the response
-      var price_data = JSON.parse(xmlHttp.responseText);
-    //   var price_data = xmlHttp.responseText.slice(1, -1);
-      console.log(price_data)
-      console.log(price_data[0])
-      console.log(price_data[1])
-    //   document.getElementById('fun-fact').innerHTML = response;
-    };
-    xmlHttp.open('GET', 'api/places', true);
-    xmlHttp.send(null);
-  }
+};
   
-  document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('submit').addEventListener('click', function() {
-        submitPlaces();
-    });
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('submit').addEventListener('click', function() {
+    var from_place = document.getElementById("from_place").value;
+    var to_place = document.getElementById("to_place").value;
+    submitPlaces(from_place, to_place);
   });
-  
+});
 
-// /*
-//  * JavaScript file for the application to demonstrate
-//  * using the API
-//  */
-
-// // Create the namespace instance
-// let ns = {};
-
-// // Create the model instance
-// ns.model = (function() {
-//     'use strict';
-
-//     let $event_pump = $('body');
-
-//     // Return the API
-//     return {
-//         'read': function() {
-//             let ajax_options = {
-//                 type: 'GET',
-//                 url: 'api/places',
-//                 accepts: 'application/json',
-//                 dataType: 'json'
-//             };
-//             $.ajax(ajax_options)
-//             .done(function(data) {
-//                 $event_pump.trigger('model_read_success', [data]);
-//             })
-//             .fail(function(xhr, textStatus, errorThrown) {
-//                 $event_pump.trigger('model_error', [xhr, textStatus, errorThrown]);
-//             })
-//         },
-//         submit: function(from_place, to_place) {
-//             let ajax_options = {
-//                 type: 'POST',
-//                 url: 'api/people',
-//                 accepts: 'application/json',
-//                 contentType: 'application/json',
-//                 dataType: 'json',
-//                 data: JSON.stringify({
-//                     'from_place': from_place,
-//                     'to_place': to_place
-//                 })
-//             };
-//             $.ajax(ajax_options)
-//             .done(function(data) {
-//                 $event_pump.trigger('model_create_success', [data]);
-//             })
-//             .fail(function(xhr, textStatus, errorThrown) {
-//                 $event_pump.trigger('model_error', [xhr, textStatus, errorThrown]);
-//             })
-//         }
-//     };
-// }());
-
-// // Create the view instance
-// ns.view = (function() {
-//     'use strict';
-
-//     let $from_place = $('#from_place'),
-//         $to_place = $('#to_place');
-
-//     // return the API
-//     return {
-//         reset: function() {
-//             $lname.val('');
-//             $fname.val('').focus();
-//         },
-//         build_table: function(people) {
-//             let rows = ''
-
-//             // clear the table
-//             $('.people table > tbody').empty();
-
-//             // did we get a people array?
-//             if (price_data) {
-//                 var data = {
-//                     labels: price_data[0],
-//                     datasets: [{
-//                       label: "Dataset #1",
-//                       backgroundColor: "rgba(255,99,132,0.2)",
-//                       borderColor: "rgba(255,99,132,1)",
-//                       borderWidth: 2,
-//                       hoverBackgroundColor: "rgba(255,99,132,0.4)",
-//                       hoverBorderColor: "rgba(255,99,132,1)",
-//                       data: price_data[1],
-//                     }]
-//                   };
-                  
-//                   var options = {
-//                     maintainAspectRatio: false,
-//                     scales: {
-//                       yAxes: [{
-//                         stacked: true,
-//                         gridLines: {
-//                           display: true,
-//                           color: "rgba(255,99,132,0.2)"
-//                         }
-//                       }],
-//                       xAxes: [{
-//                         gridLines: {
-//                           display: false
-//                         }
-//                       }]
-//                     }
-//                   };
-                  
-//                   Chart.Line('chart', {
-//                     options: options,
-//                     data: data
-//                   });
-//             }
-//         },
-//         error: function(error_msg) {
-//             $('.error')
-//                 .text(error_msg)
-//                 .css('visibility', 'visible');
-//             setTimeout(function() {
-//                 $('.error').css('visibility', 'hidden');
-//             }, 3000)
-//         }
-//     };
-// }());
-
-// // Create the controller
-// ns.controller = (function(m, v) {
-//     'use strict';
-
-//     let model = m,
-//         view = v,
-//         $event_pump = $('body'),
-//         $from_place = $('#from_place'),
-//         $to_place = $('#to_place');
-
-//     // Get the data from the model after the controller is done initializing
-//     setTimeout(function() {
-//         model.read();
-//     }, 100)
-
-//     // Validate input
-//     function validate(from_place, to_place) {
-//         return from_place !== "" && to_place !== "";
-//     }
-
-//     // Create our event handlers
-//     $('#submit').click(function(e) {
-//         let from_place = $from_place.val(),
-//             to_place = $to_place.val();
-
-//         e.preventDefault();
-
-//         if (validate(from_place, to_place)) {
-//             model.create(from_place, to_place)
-//         } else {
-//             alert('Problem with first or last name input');
-//         }
-//     });
-
-//     // $('#update').click(function(e) {
-//     //     let from_place = $from_place.val(),
-//     //         to_place = $to_place.val();
-
-//     //     e.preventDefault();
-
-//     //     if (validate(from_place, to_place)) {
-//     //         model.update(from_place, to_place)
-//     //     } else {
-//     //         alert('Problem with first or last name input');
-//     //     }
-//     //     e.preventDefault();
-//     // });
-
-//     $('#reset').click(function() {
-//         view.reset();
-//     })
-
-//     // $('table > tbody').on('dblclick', 'tr', function(e) {
-//     //     let $target = $(e.target),
-//     //         from_place,
-//     //         to_place;
-
-//     //     from_place = $target
-//     //         .parent()
-//     //         .find('td.from_place')
-//     //         .text();
-
-//     //     to_place = $target
-//     //         .parent()
-//     //         .find('td.to_place')
-//     //         .text();
-
-//     //     view.update_editor(from_place, to_place);
-//     // });
-
-//     // Handle the model events
-//     $event_pump.on('model_read_success', function(e, data) {
-//         view.build_table(data);
-//         view.reset();
-//     });
-
-//     $event_pump.on('model_create_success', function(e, data) {
-//         model.read();
-//     });
-
-//     $event_pump.on('model_update_success', function(e, data) {
-//         model.read();
-//     });
-
-//     $event_pump.on('model_delete_success', function(e, data) {
-//         model.read();
-//     });
-
-//     $event_pump.on('model_error', function(e, xhr, textStatus, errorThrown) {
-//         let error_msg = textStatus + ': ' + errorThrown + ' - ' + xhr.responseJSON.detail;
-//         view.error(error_msg);
-//         console.log(error_msg);
-//     })
-// }(ns.model, ns.view));
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('graph').addEventListener('click', function() {
+    updateGraph([[0,0], [0,0]]);
+  });
+});
