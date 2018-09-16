6085e0037c33fec97c691d83fa05920fa8ac5fda
unknown <Richard Liu@R-TRINITYFORCE.MIT.EDU>
2018-09-16 10:31:17 -0400

    make it better

--- web1.sh
+++
@@ -1,3 +0,0 @@
-curl --data 'response_type=code' 'http://web.chal.csaw.io:9000/oauth2/authorize'
-
-curl --data 'grant_type=authorization_code&code=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1MzcwNTk1MTUsImV4cCI6MTUzNzA2MDExNX0.crPx-kzjOGh5SCjT2oazG1vgM96ijc4Wzc7pgV5Kogw&redirect_uri=web.chal.csaw.io:9000' 'http://web.chal.csaw.io:9000/oauth2/token'
